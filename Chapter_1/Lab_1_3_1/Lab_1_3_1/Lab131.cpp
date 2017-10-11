95bdef7d65fa071ed7941df7a92293bcb4536e63
Маша Саранцева <syanelse@gmail.com>
2017-10-11 21:04:25 +0300

    Lab 1.2

---
+++ Chapter_1/Lab_1_3_1/Lab_1_3_1/Lab131.cpp
@@ -0,0 +1,17 @@
+// Lab131.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main() 
+{
+	int v = 3 * 60 * 60;  
+	int z = 3 * 60;// This is a variable to hold the value of 3 minutes in seconds
+	int zzz = 5 * 60;// This is a variable to hold the value of 6 minutes in seconds    
+	float pi = 3.141526;
+	std::cout << "result:  " << v <<  std::endl; // returning the value of v
+}
+
