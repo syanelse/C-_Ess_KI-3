63d6fce4faa725abe2be44455741635ae3ccd30f
Маша Саранцева <syanelse@gmail.com>
2017-10-11 20:54:03 +0300

    Lab 1.1

---
+++ Chapter_1/Lab_1_2_6/Lab_1_2_6/Lab126.cpp
@@ -0,0 +1,12 @@
+// Lab126.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main(void) 
+{
+	cout << "Hello world!" << endl;
+	cout << "It's me, your first program.\n"; 
+	return 0;
+}
