4b5b8bf58758a908afd44ec8a8feb08582217311
Маша Саранцева <syanelse@gmail.com>
2017-10-22 15:58:22 +0300

    Lab 2.3

---
+++ Chapter_2/Lab_2_1_5(3)/Lab_2_1_5(3)/Lab2153.cpp
@@ -0,0 +1,44 @@
+// Lab2153.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main(void)
+{
+	int sys;
+	float m, ft, in;
+	cout << "Input 0 for metric or 1 for imperial: ";
+	cin >> sys;
+	if (sys == 1)
+	{
+		cout << "Foot: ";
+		cin >> ft;
+		if (ft - (int)ft != 0)
+		{
+			cout << "Foots must be int!";
+			return 0;
+		}
+		cout << "Inches: ";
+		cin >> in;
+		m = ft * 0.3048;
+		m += in * 0.0254;
+		cout << m << " m" << endl;
+	}
+	else if (sys == 0)
+	{
+		cout << "Meters: ";
+		cin >> m;
+		ft = (int)(m / 0.3048);
+		m = m - ft * 0.3048;
+		in = m / 0.0254;
+		cout << ft << "'" << in << "\"" << endl;
+	}
+	else
+	{
+		cout << "Only 0 and 1 are valid values.";
+	}
+	return 0;
+}
+
