c032ad17fe05cacf1a7b06bd59fe1017761d7460
Маша Саранцева <syanelse@gmail.com>
2017-10-22 15:45:42 +0300

    Lab 2.2

---
+++ Chapter_2/Lab_2_1_5(2)/Lab_2_1_5(2)/Lab2152.cpp
@@ -0,0 +1,25 @@
+// Lab2152.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	float grossprice, taxrate, netprice, taxvalue;
+	cout << "Enter a gross price: ";
+	cin >> grossprice;
+	cout << "Enter a tax rate: ";
+	cin >> taxrate;
+	if (grossprice <= 0 || taxrate <= 0 || taxrate >= 100) 
+	{
+		cout << "The value of gross price or tax rate is invalid.";
+		return 0;
+	}
+	netprice = grossprice / ((taxrate + 100) / 100);
+	taxvalue = grossprice - netprice;
+	cout << "Net price: " << netprice << endl;
+	cout << "Tax value: " << taxvalue << endl;
+	return 0;
+}
+
